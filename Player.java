5acfd4a49ff1308d9825b007f5b0329675ef740d
Ben <benbruland@gmail.com>
2020-10-25 16:33:02 -0700

    Added class skeleton for Player.java

---
+++ Player.java
@@ -0,0 +1,73 @@
+public class Player {
+
+    /* Primitive Attributes */
+    private int playerRank;
+    private int playerCredits;
+    private int playerDollars;
+    private int rehearseBonus;
+    private int playerID;
+    private boolean playerInRole;
+
+    /* Non-Primitive Attributes */
+    private Role playerRole;
+    //TODO private Room playerRoom;
+
+
+    /*
+        ======== PRIVATE METHODS ========
+     */
+
+    // TODO Implement Upgrade()
+    private boolean upgrade() {
+        return false;
+    }
+
+    // TODO Implement Rehearse()
+    private boolean rehearse() {
+        return false;
+    }
+
+    // TODO Implement Move()
+    private boolean move() {
+        return false;
+    }
+
+    // TODO Discuss purpose of work()
+    private void work() {
+
+    }
+
+    // TODO Implement ChooseRole()
+    private boolean chooseRole() {
+        return false;
+    }
+
+    /*
+        ======== PUBLIC METHODS ========
+    */
+
+    public Role getRole() {
+        return this.playerRole;
+    }
+
+    public void setPlayerCredits(int numCredits) {
+        this.playerCredits = numCredits;
+    }
+
+    public void setPlayerDollars(int numDollars) {
+        this.playerDollars = numDollars;
+    }
+
+    public int getPlayerDollars() {
+        return this.playerDollars;
+    }
+
+    public int getPlayerCredits() {
+        return this.playerCredits;
+    }
+
+    // TODO Implement AddRehearsalBonus()
+    public void addRehearsalBonus() {
+
+    }
+}
