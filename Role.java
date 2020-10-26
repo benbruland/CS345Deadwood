ad1a682a5fab0c5e26df284278fd5d843cefea19
lmcintosh623 <lmcintosh623@gmail.com>
2020-10-25 21:29:48 -0700

    fixed bug in Role.java: "string" --> "String".

---
+++ Role.java
@@ -0,0 +1,21 @@
+public class Role {
+
+    private int roleID;
+    private String roleName;
+    private boolean roleAvailable;
+    private boolean isOnCardRole;
+
+    //TODO Implement Role Constructor
+    public Role() {
+
+    }
+
+    public boolean getRoleAvailable() {
+        return this.roleAvailable;
+    }
+
+    //TODO Implement SetRoleAvailable
+    public void setRoleAvailable(boolean isAvailable) {
+        this.roleAvailable = isAvailable;
+    }
+}
